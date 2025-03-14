554387606e4b294018ceba847934c7ceda3710cd
nomangul <nomangul2001@gmail.com>
2025-03-14 07:24:42 +0500

    Add static file serving and Vercel configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const grammarRoutes = require("./routes/grammar");
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/grammar", grammarRoutes);
+
+app.use("/", express.static(path.join(path.resolve(), "./client/dist")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
